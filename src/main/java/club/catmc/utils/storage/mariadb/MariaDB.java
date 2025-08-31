aa330406ce7d7db01a2c8372914124244d35f1ed
virtualWinter <admin@arsn.cc>
2025-08-31 05:00:40 +0300

    feat: add postgres, mysql, and mariadb support

---
+++ src/main/java/club/catmc/utils/storage/mariadb/MariaDB.java
@@ -0,0 +1,76 @@
+package club.catmc.utils.storage.mariadb;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MariaDB {
+
+    private String url;
+    private String host;
+    private int port;
+    private String databaseName;
+    private String username;
+    private String password;
+    private boolean usePooling;
+    private Connection connection;
+    private HikariDataSource dataSource;
+
+    public MariaDB(String url, boolean usePooling) {
+        this.url = url;
+        this.usePooling = usePooling;
+    }
+
+    public MariaDB(String host, int port, String databaseName, String username, String password, boolean usePooling) {
+        this.host = host;
+        this.port = port;
+        this.databaseName = databaseName;
+        this.username = username;
+        this.password = password;
+        this.usePooling = usePooling;
+    }
+
+    public void init() throws SQLException {
+        if (usePooling) {
+            HikariConfig config = new HikariConfig();
+            if (url != null) {
+                config.setJdbcUrl(url);
+            } else {
+                config.setJdbcUrl("jdbc:mariadb://" + host + ":" + port + "/" + databaseName);
+                config.setUsername(username);
+                config.setPassword(password);
+            }
+            this.dataSource = new HikariDataSource(config);
+        } else {
+            if (url != null) {
+                this.connection = DriverManager.getConnection(url);
+            } else {
+                String url = "jdbc:mariadb://" + host + ":" + port + "/" + databaseName;
+                this.connection = DriverManager.getConnection(url, username, password);
+            }
+        }
+    }
+
+    public void end() throws SQLException {
+        if (usePooling) {
+            if (this.dataSource != null && !this.dataSource.isClosed()) {
+                this.dataSource.close();
+            }
+        } else {
+            if (this.connection != null && !this.connection.isClosed()) {
+                this.connection.close();
+            }
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        if (usePooling) {
+            return dataSource.getConnection();
+        } else {
+            return connection;
+        }
+    }
+}
